f3c9b9340af1aa7f202f02a22a1c8fd5353da90d
Yasith Ishara Perera <yasithishara@gmail.com>
2024-08-06 19:28:00 +0530

    Added 6 batch records

--- db.sql
+++ db.sql
@@ -3,3 +3,11 @@ CREATE TABLE batch
     number     VARCHAR(10) PRIMARY KEY,
     program_id VARCHAR(10) NOT NULL REFERENCES program (id)
 );
+
+INSERT INTO batch (number,program_id) VALUES
+                                          ("B001","P001"),
+                                          ("B002","P001"),
+                                          ("B003","P002"),
+                                          ("B004","P002"),
+                                          ("B005","P002"),
+                                          ("B006","P003");
