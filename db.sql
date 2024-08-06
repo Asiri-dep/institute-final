cc1c089a17f74021a462dd1cd7e165de7e5c915b
Yasith Ishara Perera <yasithishara@gmail.com>
2024-08-06 20:41:09 +0530

    Changed the batch table

--- db.sql
+++ db.sql
@@ -36,12 +36,12 @@ INSERT INTO Module (id, name, credits, program_id) VALUES
                                                        ('M004', 'Database Systems', 3, 'P002');
 
 INSERT INTO batch (number, program_id)
-VALUES ("B001", "P001"),
-       ("B002", "P001"),
-       ("B003", "P002"),
-       ("B004", "P002"),
-       ("B005", "P002"),
-       ("B006", "P003");
+VALUES ('B001', 'P001'),
+       ('B002', 'P001'),
+       ('B003', 'P002'),
+       ('B004', 'P002'),
+       ('B005', 'P002'),
+       ('B006', 'P003');
 
 CREATE TABLE enrollment
 (
